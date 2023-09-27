543b38774848998fa1ce132ed492f7870905e361
TomoyaWaniko13 <twaniko@northislandcollege.ca>
2023-09-27 09:46:26 -0700

    add comment to explain an important point

--- src/zyBooks/Loops_4/AsteriskSeparatedValues.java
+++ src/zyBooks/Loops_4/AsteriskSeparatedValues.java
@@ -1,2 +1,28 @@
-package zyBooks.Loops_4;public class AsteriskSeparatedValues {
+package zyBooks.Loops_4;
+
+import java.util.Scanner;
+
+public class AsteriskSeparatedValues {
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        int numInput;
+
+        /* Your code goes here */
+        int value;
+
+        numInput = scnr.nextInt();
+        String s = "";
+        for (int i = 0; i < numInput; i++) {
+            value = scnr.nextInt();
+
+            System.out.print(value);
+
+            //except the last iteration
+            if (i != numInput - 1) {
+                System.out.print("*");
+            }
+        }
+
+        System.out.println();
+    }
 }
